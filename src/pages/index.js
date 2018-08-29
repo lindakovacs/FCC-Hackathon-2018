b854ef0e45430cc281fe308d10a5bbc801b2bad4
Linda Kovacs <lindaprovenza@yahoo.com>
2018-08-28 22:09:05 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,11 @@ const IndexPage = () => (
     <h1> 2018 freeCodeCamp JAMstack Hackathon!!!!</h1>
     <h2>October 27-28, 2018 9AM-9PM at GitHub in San Francisco and online </h2>
     <h3>My name is Linda Kovacs </h3>
-    <p> I am Remote Freelancer - Web Developer, Web Designer, and Graphic Designer</p>
+    <p> I am a Remote Freelancer - Web Developer, Web Designer, and Graphic Designer</p>
     <p>I was always interested in computers and I decided to have a job by programming them. 
       As a designer I taught myself HTML, CSS, and JavaScript but I really learned how to program at FreeCodeCamp. 
       FreeCodeCamp changed my life: I had to invest a lot of my spare time to work thouth the challanges, but it paid off considerably. 
-      I think we should all tlearn to code using FreeCodeCamp platform! </p>
+      I think we should all learn to code using FreeCodeCamp platform! </p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
